83bddd049e245fcaeb65681790de50755ce39c56
laraanna <lara@laraannawagner.com>
2017-11-26 15:26:37 +0100

    Pass the Tests

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -1,4 +1,5 @@
 class Codebreaker
+
   class Game
     attr_reader :output
 
@@ -8,16 +9,39 @@ class Codebreaker
 
     def start(secret_number)
       @secret_number = secret_number
+      @secret_array = secret_number.split('')
 
       output.puts "Welcome to Codebreaker"
       output.puts "Enter guess:"
+
     end
 
     def guess(input)
-      # Make sure to replace next line with the actual implemented marking algorithm,
-      # using the @secret_number
-
-      output.puts "you typed '#{input}'"
+      unless input.length == @secret_number.length
+        output.puts "Try guessing a number with four digits"
+      else
+        count_matches(input)
+        display_guesses
+      end
     end
+
+    def count_matches(input)
+      @input_array = input.split('')
+      @exact_match = 0
+      @number_match = 0
+
+      @secret_array.each_with_index do |s_number, index|
+		      if @input_array[index] == s_number
+			         @exact_match += 1
+		      elsif s_number == input[s_number]
+			         @number_match += 1
+		      end
+	    end
+    end
+
+    def display_guesses
+      output.puts (@number_match + @exact_match == 0) ? "" : "+" * @exact_match + "-" * @number_match
+    end
+
   end
 end
